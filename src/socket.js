4150343c7cc7d1cba8784f760c23714352e4c586
Nguyen Luan <nguyenluan14099@gmail.com>
2024-05-12 22:08:56 +0700

    updateUrlSocket

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "ws://localhost:8000";
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : "https://endsocketne.vercel.app";
 
 export const socket = io(URL, {
   autoConnect: false,

f6ccdc5c451ea63b26205237028ef208e41d238f
Nguyen Luan <nguyenluan14099@gmail.com>
2024-05-13 15:51:37 +0700

    URLsocket01

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production"
     ? undefined
-    : "https://endsocketne.vercel.app/";
+    : "https://endsocketne.vercel.app";
 export const socket = io(URL, {
   autoConnect: false,
 });
